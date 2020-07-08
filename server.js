632ee6b794f4c49a6284d4dbac1328cce35f2ac6
akolosov <jora364@gmail.com>
2020-07-08 20:50:15 +0300

    add hash

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require("express");
+const serveStatic = require("serve-static");
+const bodyParser = require("body-parser");
+
+let current_hash = "31415";
 
 let app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded());
+app.use((req, res, next) => {
+	if (req.originalUrl === "/" || req.originalUrl === "/index.html") {
+		res.send("invalid hash");
+	}
+	if (req.originalUrl.includes("?hash")) {
+		if (req.originalUrl === "/?hash=" + current_hash) {
+			next();
+		} else {
+			res.send("invalid hash");
+		}
+		return;
+	}
+	next();
+});
+
 app.use(serveStatic(__dirname + "/dist"));
+
+app.post("/update", (req, res) => {
+	current_hash = req.body.hash;
+	res.status(200);
+	res.send("new url - http://dota-pick-tester.herokuapp.com/?hash=" + req.body.hash);
+});
+
+app.get("/generate_hash", (req, res) => {
+	res.send(`
+		<!DOCTYPE html>
+		<html>
+		<body>
+			<form method="post" action="update">
+				<input type="text" name="hash" value="${current_hash}"/>
+				<button>update hash</button>
+			</form>
+		</body>
+		</html>
+	`);
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Listening on port ' + port)
+	console.log("Listening on port " + port)
 });
+
